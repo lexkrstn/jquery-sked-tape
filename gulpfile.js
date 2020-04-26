b543bf095ffb2832bbd19cf866f96f9c0ad1a64a
Alexander Korostin <coderlex@gmail.com>
2020-04-26 23:19:15 +0300

    Added artifacts minification

--- gulpfile.js
+++ gulpfile.js
@@ -10,9 +10,9 @@ const gulp            = require('gulp'),
 	  open            = require('gulp-open'),
 	  wrap            = require('gulp-wrap'),
 	  headerComment   = require('gulp-header-comment'),
+	  rename          = require('gulp-rename'),
+	  terser          = require('gulp-terser'),
 	  stripCssComments = require('gulp-strip-css-comments')
-
-const DEBUG = ['prod', 'production'].indexOf(process.env.NODE_ENV) < 0
 
 let TASK_NOTIFICATION = false,
 	LIVE_RELOAD = false
@@ -23,15 +23,13 @@ const plumberErrorHandler = {
 		message: 'Error: <%= error.message %>'
 	})
 }
-      
-gulp.task('sass', () => {
-	let main = gulp.src('src/jquery.skedTape.sass')
+
+const buildSass = (minify) => () => {
+	return gulp.src('src/jquery.skedTape.sass')
 		.pipe(plumber(plumberErrorHandler))
-	if (DEBUG) {
-		main = main.pipe(sourcemaps.init())
-	}
-	main = main.pipe(sass({
-			outputStyle: 'expanded',
+		.pipe(gulpif(minify, sourcemaps.init()))
+		.pipe(sass({
+			outputStyle: minify ? 'compressed' : 'expanded',
 			sourcemap: true,
 			includePaths: [
 				//__dirname + '/node_modules/foundation-sites/scss'
@@ -45,29 +43,37 @@ gulp.task('sass', () => {
 			License: <%= pkg.license %>
 			Author: <%= pkg.author %>
 		`))
-	if (DEBUG) {
-		main = main.pipe(sourcemaps.write('.'))
-	}
-	// Remove piped files so that if there was an error
-	// their old versions wont exist.
-	return main.pipe(cleanDest('dist'))
+		.pipe(gulpif(minify, rename({suffix: '.min'})))
+		.pipe(gulpif(minify, sourcemaps.write('.')))
+		// Remove piped files so that in case of an error their old versions don't exist.
+		.pipe(cleanDest('dist'))
 		.pipe(gulp.dest('dist'))
 		.pipe(gulpif(TASK_NOTIFICATION, notify({message: 'SASS built'})))
-})
+}
+      
+gulp.task('sass', buildSass(false))
+gulp.task('sass:min', buildSass(true))
 
-gulp.task('copy-js', () =>
-	gulp.src(`src/*.js`)
-        .pipe(plumber(plumberErrorHandler))
+const copyJs = (minify) => () => {
+	return gulp.src(`src/*.js`)
+		.pipe(plumber(plumberErrorHandler))
 		.pipe(cleanDest('dist'))
+		.pipe(gulpif(minify, sourcemaps.init()))
 		.pipe(wrap({ src: './umd.template.txt' }))
+		.pipe(gulpif(minify, terser()))
 		.pipe(headerComment(`
 			jQuery.skedTape v<%= pkg.version %>
 			License: <%= pkg.license %>
 			Author: <%= pkg.author %>
 		`))
+		.pipe(gulpif(minify, rename({suffix: '.min'})))
+		.pipe(gulpif(minify, sourcemaps.write('.')))
 		.pipe(gulp.dest('dist'))
 		.pipe(gulpif(TASK_NOTIFICATION, notify({message: 'JS copied'})))
-)
+}
+
+gulp.task('copy-js', copyJs(false))
+gulp.task('copy-js:min', copyJs(true))
 
 gulp.task('dist-to-docs', () =>
 	gulp.src(`dist/*`)
@@ -87,15 +93,15 @@ gulp.task('watch', () => {
 		port: 8080,
 		livereload: true
 	});
-	gulp.watch('src/*.sass', gulp.parallel('sass'))
-	gulp.watch('src/*.js', gulp.parallel('copy-js'))
+	gulp.watch('src/*.sass', gulp.parallel('sass', 'sass:min'))
+	gulp.watch('src/*.js', gulp.parallel('copy-js', 'copy-js:min'))
 	gulp.watch('dist/*', gulp.parallel('dist-to-docs'))
 	// Open browser
 	gulp.src(__filename).pipe(open({uri: 'http://localhost:8080'}))
 })
 
 gulp.task('build', gulp.series(
-	gulp.parallel('sass', 'copy-js'),
+	gulp.parallel('sass', 'sass:min', 'copy-js', 'copy-js:min'),
 	'dist-to-docs'
 ))
 
